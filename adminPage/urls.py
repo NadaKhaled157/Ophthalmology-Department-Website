566391f13046bcb8964c1e21a77fd4e0ecf457cf
Nouran-Hani <shouranshouran2000@gmail.com>
2024-05-21 04:21:31 +0300

    Editing, Deleting and sorting staff

--- adminPage/urls.py
+++ adminPage/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
 
-from django.conf import settings
-
 urlpatterns=[
-    path('dashboard/', views.admin_profile, name = "admin_profile"),
-    path('hire', views.hire, name = "hire"),
-    path('remove/<int:item_id>/', views.remove_staff, name='remove_staff'),
+    path('', views.admin_profile, name = "admin_profile"),
+    path('hire/', views.hire, name = "hire"),
+    path('fire/<int:id>/', views.fire, name = 'fire'),
+    path('edit_emp/<int:id>/', views.edit_emp, name = "edit_emp"),
 ]
--- adminPage/views.py
+++ adminPage/views.py
@@ -1,27 +1,25 @@
+from urllib import request
 from django.shortcuts import render, HttpResponse, redirect
 from django.db import connection
-from django.http import JsonResponse
 
 # Create your views here.
 
 def admin_profile(request):
+    order = request.GET.get('group', 'eid')
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT eid, fname, lname, role, salary
+                        FROM staff
+                        ORDER BY """ + order)
+        data = cursor.fetchall()
     with connection.cursor() as cursor:
         cursor.execute("""SELECT username 
                         FROM admin""")
         admin = cursor.fetchone()
     return render(request, 'adminPage/admin_profile.html', {'name': admin,
-                                                            'staff': view_staff(request)})
-
-def view_staff(request):
-    with connection.cursor() as cursor:
-        cursor.execute("""SELECT eid, fname, lname, role, salary
-                        FROM staff""")
-        data = cursor.fetchall()
-    return data
+                                                            'staff': data})
 
 def hire(request):
     if request.method == 'POST':
-        # return HttpResponse("in post")
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         phone_number = request.POST.get('number')
@@ -39,10 +37,20 @@ def hire(request):
         return redirect("admin_profile")
     return render(request, 'adminPage/add_emp.html')
 
-def remove_staff(request, id):
+def edit_emp(request, id):
     if request.method == "POST":
+        role = request.POST.get('role')
+        salary = request.POST.get('salary')
         with connection.cursor() as cursor:
-            cursor.execute("""DELETE FROM staff
-                            WHERE eid = %s""", [id])
-        return JsonResponse({'success': True})
-    return JsonResponse({'success': False})
+            cursor.execute("""UPDATE staff
+                           set role = %s, salary = %s
+                           WHERE eid = %s""", [role, salary, id])
+        return redirect("admin_profile")
+    return render(request, 'adminPage/edit_emp.html', {'id':id})
+
+def fire(request, id):
+    with connection.cursor() as cursor:
+        cursor.execute("""DELETE FROM staff
+                        WHERE eid = %s""", [id])
+    return redirect('admin_profile')
+
