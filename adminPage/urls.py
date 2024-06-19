4225ec9357154da7ac4d11829918253def585f49
Nouran-Hani <shouranshouran2000@gmail.com>
2024-06-19 18:05:20 +0300

    Admin is done

--- adminPage/urls.py
+++ adminPage/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns=[
     path('', views.admin_profile, name = "admin_profile"),
-    path('fire/<int:id>/', views.fire, name = 'fire'),
-    path('edit_emp/<int:id>/', views.edit_emp, name = "edit_emp"),
+    # path('fire/<int:id>/', views.fire, name = 'fire'),
+    # path('edit_emp/<int:id>/', views.edit_emp, name = "edit_emp"),
     path('role/', views.role, name = "role"),
     path('add_doc/', views.add_doc, name = "add_doc"),
     path('add_nur/', views.add_nur, name = "add_nur"),
@@ -13,5 +13,7 @@ urlpatterns=[
     path('rmv_doc/<int:id>/', views.rmv_doc, name = 'rmv_doc'),
     path('rmv_nur/<int:id>/', views.rmv_nur, name = 'rmv_nur'),
     path('rmv_tech/<int:id>/', views.rmv_tech, name = 'rmv_tech'),
-
+    path('edit_nur/<int:id>/', views.edit_nur, name = "edit_nur"),
+    path('edit_doc/<int:id>/', views.edit_doc, name = "edit_doc"),
+    path('edit_tech/<int:id>/', views.edit_tech, name = "edit_tech"),
 ]
--- adminPage/views.py
+++ adminPage/views.py
@@ -1,10 +1,12 @@
 from urllib import request
 from django.shortcuts import render, HttpResponse, redirect
 from django.db import connection
+from django.contrib import messages
 
 # Create your views here.
 
 def admin_profile(request):
+
     # Ordering and viewing the staff
     order = request.GET.get('group', 'eid')
     with connection.cursor() as cursor:
@@ -14,60 +16,63 @@ def admin_profile(request):
         data = cursor.fetchall()
 
     # Admin name
-    with connection.cursor() as cursor:
         cursor.execute("""SELECT username 
                         FROM admin""")
         admin = cursor.fetchone()
 
     # Getting patients number
-    with connection.cursor() as cursor:
         cursor.execute("""SELECT count(*)
                        FROM patient""")
         patients = cursor.fetchone()
 
     # Getting doctors number
-    with connection.cursor() as cursor:
         cursor.execute("""SELECT count(*)
                        FROM doctor""")
         doctors = cursor.fetchone()
 
     # Getting Nurses number
-    with connection.cursor() as cursor:
         cursor.execute("""SELECT count(*)
                        FROM nurse""")
         nurses = cursor.fetchone()
 
     # Getting Technicians number
-    with connection.cursor() as cursor:
         cursor.execute("""SELECT count(*)
                        FROM technician""")
         tech = cursor.fetchone()
 
     # Getting Staff number
-    with connection.cursor() as cursor:
         cursor.execute("""SELECT count(*)
                        FROM staff""")
         staff = cursor.fetchone()
 
     # Getting Appointments number
-    with connection.cursor() as cursor:
         cursor.execute("""SELECT count(*)
                        FROM appointment""")
         app = cursor.fetchone()
 
-    view, viewed = None, None
     if request.method == 'POST':
         view = request.POST.get('view')
-        if view == 'patient':
-            viewed = patient_view('pid')
-        elif view == 'doctor':
-            viewed = doctor_view('did')
-        elif view == 'nurse':
-            viewed = nurse_view('nid')
-        elif view == 'technician':
-            viewed = tech_view('tid')
-        elif view == 'appointment':
-            viewed = app_view()
+        request.session['role'] = view
+
+    view = request.session.get("role")
+    if request.session.get("role") == 'patient':
+        viewed = patient_view('pid')
+    elif request.session.get("role") == 'doctor':
+        viewed = doctor_view('did')
+    elif request.session.get("role") == 'nurse':
+        viewed = nurse_view('nid')
+    elif request.session.get("role") == 'technician':
+        viewed = tech_view('tid')
+    elif request.session.get("role") == 'appointment':
+        viewed = app_view()
+    elif request.session.get("role") == 'remove':
+        view, viewed = None, None
+        # Clear the session
+        request.session.flush()
+        request.session.modified = True
+    else:
+        view, viewed = None, None
+
     return render(request, 'adminPage/admin_profile.html', {'name': admin,
                                                             'patient':patients,
                                                             'doctorn':doctors,
@@ -90,7 +95,7 @@ def patient_view(order):
 def doctor_view(order):
     with connection.cursor() as cursor:
         cursor.execute("""SELECT d.did, fname, lname, age, d_specialization, d.email, s.salary
-                       FROM doctor d LEFT JOIN staff s ON d.did = s.eid
+                       FROM doctor d LEFT JOIN staff s ON d.eid = s.eid
                        ORDER BY """ + order)
         doctors = cursor.fetchall()
     return doctors
@@ -119,11 +124,10 @@ def tech_view(order):
         tech = cursor.fetchall()
     return tech
 
-# not working
 def app_view():
     with connection.cursor() as cursor:
         cursor.execute("""SELECT t3.aid, t3.p_fname, t3.p_lname, t3.fname, t3.lname, t3.app_date, t3.app_time, t3.rnum, t3.app_type, b.amount
-                        FROM (SELECT t1.aid, t1.app_date, t1.app_time, t1.app_type, t1.bid, t1.did, t1.rnum, t1.p_fname, t1.p_lname, t2.fname, t2.lname
+                        FROM (SELECT t1.aid, t1.app_date, t1.app_time, t1.app_type, t1.bid, t1.rnum, t1.p_fname, t1.p_lname, t2.fname, t2.lname
                             FROM (SELECT aid, app_date, app_time, app_type, bid, a.did, rnum, p_fname, p_lname
                                 FROM appointment a JOIN patient p ON a.pid = p.pid) AS t1
                             JOIN (SELECT fname, lname, did
@@ -148,16 +152,20 @@ def add_doc(request):
         age = request.POST.get('age')
         salary = request.POST.get('salary')
         special = request.POST.get('speicalization')
+    
         with connection.cursor() as cursor:
+            # Inserting new dr to staff table
             cursor.execute("""INSERT INTO staff (role, fname, lname, sex, age, salary, address)
             VALUES('Doctor' ,%s,%s,%s,%s,%s,%s)""",
             [first_name, last_name, gender, age, salary, address])
-        with connection.cursor() as cursor:
+
+            # Selecting dr id
             cursor.execute("""SELECT eid
                                FROM staff
                                WHERE fname = %s AND lname = %s""", [first_name, last_name])
             eid = cursor.fetchone()
-        with connection.cursor() as cursor:
+
+            # Inserting new dr to dr table
             cursor.execute("""INSERT INTO doctor (d_phone, email, password, d_specialization, eid)
                         VALUES(%s, %s, %s, %s, %s)""", [phone_number, email, password, special, eid])
         return redirect('admin_profile')
@@ -173,17 +181,21 @@ def add_nur(request):
         salary = request.POST.get('salary')
         did = request.POST.get('did')
         special = request.POST.get('speicalization')
+
         with connection.cursor() as cursor:
+            # Inserting new nurse to staff table
             cursor.execute("""INSERT INTO staff (role, fname, lname, sex, age, salary, address)
             VALUES('Nurse',%s,%s,%s,%s,%s,%s)""",
             [first_name, last_name, gender, age, salary, address])
-        with connection.cursor() as cursor:
+            
+            # Selecting nurse id
             cursor.execute("""SELECT eid
                            FROM staff
                            WHERE fname = %s and lname = %s""", [first_name, last_name])
             eid = cursor.fetchone()
-        with connection.cursor() as cursor:
-                cursor.execute("""INSERT INTO nurse (eid, did, n_specialization)
+
+            # Inserting nurse to nurse table
+            cursor.execute("""INSERT INTO nurse (eid, did, n_specialization)
                             VALUES(%s, %s, %s)""", [eid, did, special])
         return redirect('admin_profile')
     return render(request, 'adminPage/add_nur.html')
@@ -198,83 +210,173 @@ def add_tech(request):
         salary = request.POST.get('salary')
         did = request.POST.get('did')
         with connection.cursor() as cursor:
+            # Inserting new tech to staff table
             cursor.execute("""INSERT INTO staff (role, fname, lname, sex, age, salary, address)
             VALUES('Technician',%s,%s,%s,%s,%s,%s)""",
             [first_name, last_name, gender, age, salary, address])
-        with connection.cursor() as cursor:
+
+            # Selecting tech id
             cursor.execute("""SELECT eid
                            FROM staff
                            WHERE fname = %s and lname = %s""", [first_name, last_name])
             eid = cursor.fetchone()
-        with connection.cursor() as cursor:
-                cursor.execute("""INSERT INTO technician (eid, did)
+        
+            # Inserting tech to tech table
+            cursor.execute("""INSERT INTO technician (eid, did)
                             VALUES(%s, %s)""", [eid, did])
         return redirect('admin_profile')
     return render(request, 'adminPage/add_tech.html')
 
-def edit_emp(request, id):
-    if request.method == "POST":
-        role = request.POST.get('role')
+def edit_doc(request, id):
+    # Getting nurse data
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT d.eid, fname, lname, address, s.age, d_specialization, salary, email, password, d_phone
+                       From doctor d JOIN staff s ON d.eid = s.eid
+                       WHERE did = %s""", [id])
+        data = cursor.fetchone()
+        
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        address = request.POST.get('address')
+        age = request.POST.get('age')
+        phone = request.POST.get('phone')
         salary = request.POST.get('salary')
+        special = request.POST.get('speicalization')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
         with connection.cursor() as cursor:
-            cursor.execute("""UPDATE staff
-                           set role = %s, salary = %s
-                           WHERE eid = %s""", [role, salary, id])
-        return redirect("admin_profile")
-    return render(request, 'adminPage/edit_emp.html', {'id':id})
+            # Updating doctor in staff table
+            cursor.execute("""UPDATE staff 
+                            SET fname = %s, lname = %s, age = %s, salary = %s, address = %s
+                            WHERE eid = %s""",
+                            [first_name, last_name, age, salary, address, data[0]])
+            
+            # Updating doctor in dotcor table
+            cursor.execute("""UPDATE doctor
+                           SET d_specialization = %s, d_phone = %s, email = %s, password = %s
+                           WHERE did = %s""", [special, phone, email, password, id])
+
+        return redirect('admin_profile')
+    return render(request, 'adminPage/edit_doc.html', {'did': id,
+                                                       'doc': data})
+
+def edit_nur(request, id):
+    # Getting nurse data
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT n.eid, fname, lname, address, s.age, n_specialization, n.did, salary
+                       From nurse n JOIN staff s ON n.eid = s.eid
+                       WHERE nid = %s""", [id])
+        data = cursor.fetchone()
+        
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        address = request.POST.get('address')
+        age = request.POST.get('age')
+        salary = request.POST.get('salary')
+        did = request.POST.get('did')
+        special = request.POST.get('speicalization')
+
+        with connection.cursor() as cursor:
+            # Updating nurse in staff table
+            cursor.execute("""UPDATE staff 
+                            SET fname = %s, lname = %s, age = %s, salary = %s, address = %s
+                            WHERE eid = %s""",
+                            [first_name, last_name, age, salary, address, data[0]])
+            
+            # Updating nurse in nurse table
+            cursor.execute("""UPDATE nurse
+                           SET did = %s, n_specialization = %s
+                           WHERE nid = %s""", [did, special, id])
+
+        return redirect('admin_profile')
+    return render(request, 'adminPage/edit_nur.html', {'nur': data,
+                                                       'nid': id})
+
+
+def edit_tech(request, id):
+    # Getting nurse data
+    with connection.cursor() as cursor:
+        cursor.execute("""SELECT t.eid, fname, lname, address, s.age, t.did, salary
+                       From technician t JOIN staff s ON t.eid = s.eid
+                       WHERE tid = %s""", [id])
+        data = cursor.fetchone()
+        
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        address = request.POST.get('address')
+        age = request.POST.get('age')
+        salary = request.POST.get('salary')
+        did = request.POST.get('did')
+
+        with connection.cursor() as cursor:
+            # Updating techinican in staff table
+            cursor.execute("""UPDATE staff 
+                            SET fname = %s, lname = %s, age = %s, salary = %s, address = %s
+                            WHERE eid = %s""",
+                            [first_name, last_name, age, salary, address, data[0]])
+            
+            # Updating tech in tech table
+            cursor.execute("""UPDATE technician
+                           SET did = %s
+                           WHERE tid = %s""", [did, id])
+
+        return redirect('admin_profile')
+    return render(request, 'adminPage/edit_tech.html', {'tid': id,
+                                                        'tech':data})
 
 def rmv_doc(request, id):
     with connection.cursor() as cursor:
+        # Selecting dr eid
         cursor.execute("""SELECT eid
                        FROM doctor
                        WHERE did = %s""", [id])
         eid = cursor.fetchone()
 
-    with connection.cursor() as cursor:
+        # Deleting dr from dr table
         cursor.execute("""DELETE FROM doctor
                         WHERE did = %s""", [id])
         
-    with connection.cursor() as cursor:
+        # Deleting dr from staff table
         cursor.execute("""DELETE FROM staff
                         WHERE eid = %s""", [eid])
     return redirect('admin_profile')
 
 def rmv_nur(request, id):
     with connection.cursor() as cursor:
+        # Selecting nurse eid
         cursor.execute("""SELECT eid
                        FROM nurse
                        WHERE did = %s""", [id])
         eid = cursor.fetchone()
 
-    with connection.cursor() as cursor:
+        # Deleting nurse from nurse table
         cursor.execute("""DELETE FROM nurse
                         WHERE nid = %s""", [id])
         
-    with connection.cursor() as cursor:
+        # Deleting nurse from staff table
         cursor.execute("""DELETE FROM staff
                         WHERE eid = %s""", [eid])
     return redirect('admin_profile')
 
 def rmv_tech(request, id):
     with connection.cursor() as cursor:
+        # Selecting tech eid
         cursor.execute("""SELECT eid
                        FROM technician
-                       WHERE did = %s""", [id])
+                       WHERE tid = %s""", [id])
         eid = cursor.fetchone()
 
-    with connection.cursor() as cursor:
+        # Deleting tech from tech table
         cursor.execute("""DELETE FROM technician
-                        WHERE did = %s""", [id])
-        
-    with connection.cursor() as cursor:
+                        WHERE tid = %s""", [id])
+
+        # Deleting tech from staff table
         cursor.execute("""DELETE FROM staff
                         WHERE eid = %s""", [eid])
-    return redirect('admin_profile')
-
-def fire(request, id):
-    with connection.cursor() as cursor:
-        cursor.execute("""DELETE FROM staff
-                        WHERE eid = %s""", [id])
     return redirect('admin_profile')
 
 def cancel(request):
