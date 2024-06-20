6720e960cee090e36f2b75acdc5311b06174ac82
FatmaElsharkawy <fatmasaid462@yahoo.com>
2024-06-21 00:41:49 +0300

    first commit

--- MySufferingQL/settings.py
+++ MySufferingQL/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'common',
     'doctorprofile',
+    'patientprofile',
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
--- common/views.py
+++ common/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
-from django.db import connection, transaction
+from django.db import connection
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.hashers import make_password
-from django.conf import settings
 from django.urls import reverse
 from django.shortcuts import render
 from django.contrib.auth.hashers import make_password
 from django.core.files.storage import default_storage
 from django.db import connection
-from datetime import datetime
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
+
 
 
 # Create your views here.
@@ -17,13 +17,14 @@ def index(request):
     # Clearing session and cookies
     request.session.flush()
     request.session.modified = True
-    img_path = None 
+    img_path = None
     if request.method == 'POST':
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         phone_number = request.POST.get('number')
         address = request.POST.get('address')
         email = request.POST.get('email')
+        age = request.POST.get('age')
         raw_password = request.POST.get('password')
         raw_password_2 = request.POST.get('confirm-password')
         gender = request.POST.get('gender')
@@ -31,8 +32,10 @@ def index(request):
         if img:
             img_name = img.name
             img_path = default_storage.save(img_name, img)
+            request.session['img_path'] = img_path
+
         with connection.cursor() as cursor:
-           cursor.execute("SELECT Email FROM useraccount WHERE Email = %s", [email])
+           cursor.execute("SELECT Email FROM patient WHERE Email = %s", [email])
            email_db = cursor.fetchone()
            if email_db:
                 exist = "Email already exists!"
@@ -42,26 +45,29 @@ def index(request):
             with connection.cursor() as cursor:
                     ##EDIT BASED ON PATIENT TABLE##
                     cursor.execute("""
-                        INSERT INTO patient (Fname,Lname, Email, passward, address, phone, gender, image)
-                        VALUES (%s, %s, %s,%s, %s, %s, %s, %s);
-                    """, [first_name, last_name, email, password, address, phone_number, gender, img_path]) 
-                
+                        INSERT INTO patient (p_fname,p_lname, email, password, address, phone_number, sex, p_photo,p_age)
+                        VALUES (%s, %s, %s,%s, %s, %s, %s, %s,  %s);
+                    """, [first_name, last_name, email, password, address, phone_number, gender, img_path, age])
+
             with connection.cursor() as cursor:
                 ##EDIT BASED ON PATIENT TABLE##
-                cursor.execute("""SELECT id
-                               FROM useraccount
+                cursor.execute("""SELECT pid, p_fname
+                               FROM patient
                                WHERE email = %s""", [email])
-                id = cursor.fetchone()
+                id, name = cursor.fetchone()
             request.session['id']=id
-            return redirect('profile')
+            request.session['name']=name
+
+            return redirect('patientprofile:patient-profile')
         else:
             notmatch = "Passwords didn't match!"
             return render(request, "common/register.html", {'match': notmatch})
             # return HttpResponse('''Password is incorrect.''')
-    return render(request, "common/register.html")
+    else:
+        return render(request, "common/register.html")
 
 def login(request):
-    return render(request, "common/login.html")
+     return render(request, "common/login.html")
 
 def authenticate_user(request):
     # Clearing session and cookies
@@ -71,27 +77,30 @@ def authenticate_user(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
-
+        role = request.POST.get('role')
+        if role == None: return render(request, "common/login.html",{'no_role': True})
         with connection.cursor() as cursor:
-            ##Add if condition of radio button (is user a patient or doctor?)##
-            cursor.execute("SELECT * FROM useraccount WHERE email = %s", [email])
-            ##
+            cursor.execute(f"SELECT * FROM {role} WHERE email = %s", [email])
             user = cursor.fetchone()
-
         if user:
-            ##Add if condition of radio button (is user a patient or doctor?)##
-            ##and get password index from the different tables to validate password##
-            hashed_password = user[4]
-            ##
+            hashed_password = user[9]
+
             if check_password(password, hashed_password):
-                request.session['id']=[user[0]]
-                return redirect('profile')
+                request.session['id']=user[3] ##What is this??##
+                request.session['name']= user[0]
+                patient_photo= user[5].tobytes()
+                img_path=patient_photo.decode('utf-8')
+                request.session['img_path']=img_path
+                if role == "doctor":
+                    return redirect(reverse('doctorprofile:doctor-page')+ f'?doctor_id={user[0]}') #user[0] is did
+                if role == "patient":
+                    return redirect(reverse('patientprofile:patient-profile')+ f'?patient_id={user[3]}') ##pid
+
             else:
                 wrong_pass = "Wrong Password"
-                redirect('login-page')
-                return render(request, "common/login.html",{'wrong': wrong_pass})
+                redirect('common:authenticate_user')
+                return render(request, "common/login.html",{'wrong_pass': True})
 
         else:
             wrong_email = "This user does not exist"
-            return render(request, "common/login.html",{'wrong': wrong_email})
-
+            return render(request, "common/login.html",{'wrong_email': True})
